30ed5764510d7074c604f47d66a5a539314470a9
Thibaut de Saivre <thibaut.de-saivre@polytechnique.edu>
2024-02-01 02:31:33 +0100

    change the memory structure to make jump address editing possible

--- lib/src/compiler.rs
+++ lib/src/compiler.rs
@@ -1,11 +1,14 @@
 //! JIT compiler implementation
 
 use crate::instructions::Instruction;
-use memmap::MmapMut;
+use memmap::{Mmap, MmapMut};
 
 pub struct Compiler {
     /// Buffer for the generated machine code
     machine_code: Vec<u8>,
+
+    /// Actual executable memory. The jump addresses will have to be resolved here.
+    executable_memory: Mmap,
 
     /// Memory buffer of 30_000 bytes
     memory: Vec<u8>,
@@ -15,6 +18,7 @@ impl Compiler {
     pub fn new() -> Self {
         Self {
             machine_code: Vec::new(),
+            executable_memory: MmapMut::map_anon(1).unwrap().make_exec().unwrap(),
             memory: vec![0; 30_000],
         }
     }
@@ -33,36 +37,43 @@ impl Compiler {
         for instruction in source {
             // Add each instruction's corresponding byte slice to the machine code
             self.machine_code.extend_from_slice(instruction.into());
-        }
 
-        // NOTE: ensuite, il faudra utiliser une enum wrapper pour modifier le vecteur plusieurs fois, et produire un code théorique performant, avant de le compiler
+            // TODO: record the index position of every forward and backward jump. We will use them to swap the jump addresses in the final memory
+        }
 
         // Last: append the RET instruction
         self.machine_code.push(0xc3);
+
+        // Finally: copy the machine code into the executable memory, and replace the jump instructions
+        // Create an anonymous memory map the size of our machine code
+        let mut temp_memory = MmapMut::map_anon(self.machine_code.len()).unwrap();
+        temp_memory.clone_from_slice(&self.machine_code); // Clone the machine code into it
+
+        // TODO: in this section, the memory can still be modified, but it already has is final addresses.
+        // We can set and get the final jump addresses here.
+
+        // Make the memory map executable
+        self.executable_memory = temp_memory.make_exec().unwrap();
     }
 
     /// Execute the compiled machine code
     pub fn execute(&self) {
         assert!(!self.machine_code.is_empty(), "No machine code to execute");
 
-        // Create an anonymous memory map the size of our machine code
-        let mut memory_map = MmapMut::map_anon(self.machine_code.len()).unwrap();
-        memory_map.clone_from_slice(&self.machine_code); // Clone the machine code into it
-
-        // Make the memory map executable
-        let memory_map = memory_map.make_exec().unwrap();
         // Get a pointer to the machine code
-        let func_ptr = memory_map.as_ptr();
+        let func_ptr = self.executable_memory.as_ptr();
+
         unsafe {
             let main: fn() = std::mem::transmute(func_ptr);
             main();
         }
     }
 
-    /// Clear the compiler from its previous run
+    /// Clear the compiler from its previous run (reset the memory in place)
     pub fn clear(&mut self) {
-        self.machine_code.clear();
-        self.memory = vec![0; 30_000];
+        for i in 0..30_000 {
+            self.memory[i] = 0;
+        }
     }
 }
 
